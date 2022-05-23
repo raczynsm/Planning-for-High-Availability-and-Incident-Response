d7796a5dc7bd4d64adc84ab824b92ddb7cca38a3
Marta Raczynska <marta.raczynska@vimn.com>
2022-05-23 17:29:13 +0200

    Lesson-4-demo

--- lesson-4-deploying-terraform-assets/demo/ec2.tf
+++ lesson-4-deploying-terraform-assets/demo/ec2.tf
@@ -1,6 +1,6 @@
   module "project_ec2" {
    source             = "./modules/ec2"
    instance_count     = 2
-   aws_ami            = "ami-0b9064170e32bde34"
+   aws_ami            = "ami-02541b8af977f6cdd"
    public_subnet_ids  = module.vpc.public_subnet_ids
  }
